b394b24951a5a65bddde9c3ed52626ea9de23c4f
Laurynas Kairys <laurynaskairys@gmail.com>
2020-04-06 08:57:56 +0300

    Updates

---
+++ precache-manifest.81dd47c4a7e0f04857a6af53fbd003ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9a1c6ca47e4e867dbb9778d877e2f5",
+    "url": "/affordability-calculator/index.html"
+  },
+  {
+    "revision": "a8f6070cc302a4dd1d05",
+    "url": "/affordability-calculator/static/css/main.9148f93c.chunk.css"
+  },
+  {
+    "revision": "3285988d28c01314a171",
+    "url": "/affordability-calculator/static/js/2.570e3a7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/affordability-calculator/static/js/2.570e3a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f6070cc302a4dd1d05",
+    "url": "/affordability-calculator/static/js/main.fffc340e.chunk.js"
+  },
+  {
+    "revision": "19f2410938770cf68ebb",
+    "url": "/affordability-calculator/static/js/runtime-main.f0fa218b.js"
+  },
+  {
+    "revision": "9783f2067212d0e91844b4d93e4f90bd",
+    "url": "/affordability-calculator/static/media/spritesheet.9783f206.svg"
+  }
+]);
